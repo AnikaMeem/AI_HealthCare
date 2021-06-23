d496026683df672ea3cd2a1ab50d270bb40c2677
Md. Shofiul Islam <shofiul.islam@northsouth.edu>
2021-06-23 14:20:40 +0600

    enhancement #3: project settings changed

--- smartHospital/smartHospital/settings.py
+++ smartHospital/smartHospital/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hospital',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hospital'
 ]
 
 MIDDLEWARE = [
